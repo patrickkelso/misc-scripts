629a4e54600fe34a574d1fe9b81fb237a8d504e6
Patrick S. Kelso <patrick@teamkelso.org>
2021-10-06 04:54:18 +1100

    updating QuickLog

--- ql.py
+++ ql.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
+# Adjust QuickLog to your file path
 import pymsgbox 
 from datetime import datetime
-QuickLog = "/home/psk/quick.log"
+QuickLog = "/home/psk/Documents/nc/quick.log"
 response = pymsgbox.prompt('What is on your mind?', title='quick_log')
 currentDT = datetime.now()
 TimeStamp = currentDT.strftime("%Y%m%d%H%M%S")
+HumanTime = currentDT.strftime("%A %B %Y at %-H:%M")
 f = open(QuickLog, "a")
-f.write(TimeStamp + " : " + response + "\n")
+f.write(TimeStamp + " : " + HumanTime + " : " + response + "\n")
 f.close()

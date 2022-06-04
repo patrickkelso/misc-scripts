2b708bc2d0cde15f4aa4ffea97115318bf1780b7
Patrick S. Kelso <patrick@teamkelso.org>
2022-06-04 12:30:46 +1000

    Moving variables into another file
    Signed-off-by: Patrick S. Kelso <patrick@teamkelso.org>

--- ql.py
+++ ql.py
@@ -2,11 +2,14 @@
 # Adjust QuickLog to your file path
 import pymsgbox 
 from datetime import datetime
-QuickLog = "/home/psk/Documents/nc/quick.log"
+# Create a file in the same directory as ql.py called ql_vars.py and set 'QLPATH' & 'QuickLog' appropriately. For example:
+# QLPATH = "/home/psk/quicklogs"
+# QuickLog = QLPATH + "/quick.md"
+import ql_vars
 response = pymsgbox.prompt('What is on your mind?', title='quick_log')
 currentDT = datetime.now()
 TimeStamp = currentDT.strftime("%Y%m%d%H%M%S")
 HumanTime = currentDT.strftime("%A %B %Y at %-H:%M")
-f = open(QuickLog, "a")
+f = open(ql_vars.QuickLog, "a")
 f.write(TimeStamp + " : " + HumanTime + " : " + response + "\n")
 f.close()

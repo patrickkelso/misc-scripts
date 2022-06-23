906d2f40e12478b6b6338d784517b30fda5743ca
Patrick S. Kelso <psk@linux.com>
2022-06-23 12:05:59 +1000

    adding vars example to repo
    Signed-off-by: Patrick S. Kelso <psk@linux.com>

---
+++ ql_vars.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# Adjust QuickLog to your file path
+QLPATH = "/home/psk/Documents/nc/ZK/ZK"
+QuickLog = QLPATH + "/quick.md"
+QuickTaskPATH = "/home/psk/Documents/nc/ZK/ZK/quicktasks"
